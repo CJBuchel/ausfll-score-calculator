5c3b5e06df0fd3c6eeda9325a8be025c1cac4bc3
CJBuchel <connorbuchel@gmail.com>
2023-09-16 20:49:22 +0800

    force push up wasm package

--- ausfll-rs/build/generate_wasm.rs
+++ ausfll-rs/build/generate_wasm.rs
@@ -1,10 +1,8 @@
+use std::fs;
 use std::process::Command;
 use std::ffi::OsString;
 use std::path::Path;
 use fs_extra::dir::copy;
-
-// make sure `rustup target add wasm32-unknown-unknown` has been run
-// and compile using `cargo build --target=wasm32-unknown-unknown --release`
 
 pub fn generate_wasm(outdir: &OsString) {
   let wasm_compiler_path = "ausfll-wasm";
@@ -32,7 +30,18 @@ pub fn generate_wasm(outdir: &OsString) {
   let source_dir = Path::new(wasm_compiler_path).join("ausfll_wasm_pkg");
   let dest_dir = Path::new(outdir);
 
+  // remove the gitignore becuase it's medling
+  fs::remove_file(source_dir.join(".gitignore")).expect("Failed to remove .gitignore");
+
+  // Remove the destination directory if it exists
+  if dest_dir.join("ausfll_wasm_pkg").exists() {
+    fs::remove_dir_all(dest_dir.join("ausfll_wasm_pkg")).expect("Failed to remove destination directory.");
+  }
+
   // Copy the compiled wasm package to the desired location
   copy(source_dir, dest_dir, &fs_extra::dir::CopyOptions::new())
     .expect("Failed to copy directory.");
 }
+
+
+
---
+++ schema/ausfll_wasm_pkg/ausfll_wasm_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function wasm_masterpiece_validate(a: number): number;
+export function wasm_masterpiece_score(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_exn_store(a: number): void;
